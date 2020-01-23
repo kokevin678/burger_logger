702b819c5fa7a26f85f2f063f8f2c8b875dadbfa
Chengyu Ko <kokevin678@gmail.com>
2020-01-22 18:51:29 -0800

    connection working, schema loads

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+### Seeds
+
+INSERT INTO burgers (burgerName) VALUES ("Hamburger");
+INSERT INTO burgers (burgerName) VALUES ("Cheeseburger");
+INSERT INTO burgers (burgerName) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burgerName, devoured) VALUES ("Chicken Burger ", true);
